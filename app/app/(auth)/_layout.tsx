2f1d96ca6acf49bb069ecea4a536f95b97aa1316
Caio Rodrigues <124935336+scaiorodrigues@users.noreply.github.com>
2026-06-25 17:58:41 -0300

    feat: auth layout

--- app/app/(auth)/_layout.tsx
+++ app/app/(auth)/_layout.tsx
@@ -1,13 +1,9 @@
-import { Redirect } from 'expo-router'
 import { Stack } from 'expo-router'
 import { useAuthStore } from '../../store/useAuthStore'
+import { Redirect } from 'expo-router'
 
 export default function AuthLayout() {
   const { session, loading } = useAuthStore()
-  if (!loading && session) return <Redirect href="/(app)" />
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="login" />
-    </Stack>
-  )
+  if (!loading && session) return <Redirect href="/(app)"/>
+  return <Stack screenOptions={{ headerShown: false }}/>
 }
